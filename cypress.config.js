29f50fc9497c32d7c0684d8e2a9f2ca63aee135b
Molnar Igor <igor.molnar@hyperether.com>
2023-08-22 14:58:52 +0200

    test push7

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'projectId': '4b7344',
   e2e: {
-    "baseUrl": "http://localhost:8080",
+    "baseUrl": "http://example.cypress.io",
     "env" : {
       "navbarText" : "cypress.io"
     }
